8ee2e9ac4329baf9dc96e6af44dcc98866b7b9c7
rpersaud7 <rpersaud7@hotmail.com>
2017-06-16 13:16:16 -0400

    The attempted integration of the database.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-//const pg = require('pg');
-//const conString = 'postgres://postgres:talesh100996@localhost/blog_db';
+const pg = require('pg');
+const conString = 'postgres://postgres:talesh100996@localhost/blog_db';
+const client = new pg.Client(conString);
+client.connect();
+//create tables for blog posts and comments, but only once
+const query= client.query( 'CREATE TABLE [IF NOT EXISTS] entries(title VARCHAR(255) NOT NULL, content TEXT)');
+  query.on('end', () => { client.end(); });
+const query2= client.query('CREATE TABLE [IF NOT EXISTS] comments(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL UNIQUE,created_at DATETIME NOT NULL DEFAULT');
+  query.on('end', () => { client.end(); });
 //set the view engine to hbs for handlebars, ejs for ejs, or pug for pug
 app.set('view engine', 'ejs');
 //sets this application to look at `my-views` next to the running application
@@ -13,8 +21,8 @@ app.set('views', './views');
 app.use(express.static(__dirname));
 
 app.get('/home', function(req, res){
- //renders the `home-page` view in `views`
- res.render('home-page');
+  //renders the `home-page` view in `views`
+  res.render('home-page');
 });
 
 app.get('/portfolio', function(req, res){
@@ -35,11 +43,10 @@ function redirect(){
 
 app.get('*', function(req,res){
   res.status(404).send('Page not found');
-  setTimeout(redirect(), 5000);
 });
 
 app.listen(3000, function() {
- console.log('Listening on port 3000')
+  console.log('Listening on port 3000')
 });
 
 /* Set the width of the side navigation to 250px */
@@ -54,6 +61,13 @@ function closeNav() {
 
 // function post(){
 //   pg.connect('conString', function(err, client, done){
+//        // Handle connection errors
+//    if(err) {
+//      done();
+//      console.log(err);
+//      return res.status(500).json({success: false, data: err});
+//    }
+
 //     client.query('insert into posts,bodyParser.')  //parse html form and save to database, both title and content
 //   });
 // }
