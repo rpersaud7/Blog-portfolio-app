cc2e88460f9290d2c06119fac1c62d2a0c218c71
rpersaud7 <rpersaud7@hotmail.com>
2017-06-16 14:51:42 -0400

    Integration of database and creation of table.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
 
-const pg = require('pg');
-const conString = 'postgres://postgres:talesh100996@localhost/blog_db';
-const client = new pg.Client(conString);
+var pg = require('pg');
+var conString = 'postgres://postgres:talesh100996@localhost/blogpost';
+var client = new pg.Client(conString);
 client.connect();
 //create tables for blog posts and comments, but only once
-const query= client.query( 'CREATE TABLE [IF NOT EXISTS] entries(title VARCHAR(255) NOT NULL, content TEXT)');
+var query= client.query( 'CREATE TABLE IF NOT EXISTS entries(title VARCHAR(255) NOT NULL, content TEXT)');
   query.on('end', () => { client.end(); });
-const query2= client.query('CREATE TABLE [IF NOT EXISTS] comments(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL UNIQUE,created_at DATETIME NOT NULL DEFAULT');
-  query.on('end', () => { client.end(); });
+// var query2= client.query('CREATE TABLE IF NOT EXISTS comments(id SERIAL NOT NULL PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL UNIQUE,created_at DATETIME NOT NULL DEFAULT');
+//   query.on('end', () => { client.end(); });
 //set the view engine to hbs for handlebars, ejs for ejs, or pug for pug
 app.set('view engine', 'ejs');
 //sets this application to look at `my-views` next to the running application
@@ -59,18 +63,21 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-// function post(){
-//   pg.connect('conString', function(err, client, done){
-//        // Handle connection errors
-//    if(err) {
-//      done();
-//      console.log(err);
-//      return res.status(500).json({success: false, data: err});
-//    }
+app.post('/blogpost', function(req,res){
+  //parse html form and save to database, both title and content
+  var btitle = req.body.title;
+  var bcontent = req.body.content;
+  pg.connect('conString', function(err, client, done){
+       // Handle connection errors
+   if(err) {
+     done();
+     console.log(err);
+     return res.status(500).json({success: false, data: err});
+   }
 
-//     client.query('insert into posts,bodyParser.')  //parse html form and save to database, both title and content
-//   });
-// }
+    client.query('INSERT INTO blogpost (title, content) values (btitle, bcontent)')
+  }}));
+
 
 function loadcommentposter(){
 
