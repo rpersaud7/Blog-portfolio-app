8279c3bb20b94ac5f1522e890d9df52e0f470bf9
rpersaud7 <rpersaud7@hotmail.com>
2017-06-20 14:13:35 -0400

    Finally got blog posts to load onto the page from the database.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -6,6 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended:true
 }));
+
 
 //initialize connection with sequelize then test connection
 const sequelize = new Sequelize('postgres://postgres:1029384756@localhost:5432/blogpost');
@@ -19,12 +21,18 @@ sequelize.authenticate().then(() => {
 var Entries= sequelize.define('entries',
 {
   id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
   title: Sequelize.STRING,
   content: Sequelize.STRING
+});
+
+var bentries = ["no"];
+
+Entries.findAll().then(entries => {
+  bentries = entries;
 });
 
 app.set('view engine', 'ejs');
@@ -43,16 +51,25 @@ app.get('/portfolio', function(req, res){
 })
 
 app.get('/blog', function(req, res){
-  res.render('blog');
+  Entries.findAll().then(entries => {
+      res.render('blog', {entries});
+  });
 });
 
-app.get('/blogpost', function(req, res){
-  res.render('blogpost');
+app.post('/blog', function(req, res){
+  //adds blog post to database
+  var title = req.body.title;
+  var content = req.body.content;
+  Entries.sync().then(function(){
+    Entries.create({
+      title: title,
+      content: content
+    });
+    console.log("Success!");
+  });
+  Entries.sync();
+  res.redirect('/blog')
 });
-
-function redirect(){
-  res.render('home-page');
-}
 
 app.get('*', function(req,res){
   res.status(404).send('Page not found');
@@ -72,20 +89,10 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-app.get('/blogpost', function(req,res){
-  res.render('blogpost');
-});
-
-app.post('/blogpost', function(req, res){
-  //adds blog post to database
-  var title = req.body.title;
-  var content = req.body.content;
-  Entries.sync().then(function(){
-    Entries.create({
-      title: title,
-      content: content
-    });
-    console.log("Success!");
-  });
-  Entries.sync();
-});
+// $('#submit').on('submit', function(event) {
+//   event.preventDefault();
+//   $.ajax({
+//     url: 'http://localhost:3000/blog',
+//
+//   })
+// });
