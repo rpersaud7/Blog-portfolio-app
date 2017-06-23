e5937a02c68db6c9cd99ecb8e5f6b7b1acfe3696
rpersaud7 <rpersaud7@hotmail.com>
2017-06-23 12:50:20 -0400

    Fixed port bindings.

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get('*', function(req,res){
   res.status(404).send('Page not found');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port 3000')
 });
 
